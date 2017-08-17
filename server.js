76774ff482fbae9a02827f62b5a5ab08fdaaf8ee
ctvanzandt42 <ctvanzandt42@gmail.com>
2017-08-17 14:21:46 -0500

    Added profile page and added routes

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const mustacheExpress = require('mustache-express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const path = require('path');
+const users = require('./users');
+const checkAuth = require('./middlewares/checkAuth');
 const sessionConfig = require('./sessionConfig');
 const port = process.env.PORT || 3000;
 
@@ -32,4 +34,32 @@ app.get("/login", (req, res) => {
 
 app.get('/signup', (req, res) => {
     res.render('signup');
+});
+
+app.post("/signup", (req, res) => {
+    let newUser = req.body;
+    console.log("newUser: ", newUser);
+    users.push(newUser);
+    console.log("users: ", users);
+    res.redirect("/login");
+});
+
+app.post("/login", (req, res) => {
+    let reqUsername = req.body.username;
+    let reqPassword = req.body.password;
+    let foundUser = users.find(user => user.username === reqUsername);
+    if (!foundUser) {
+        return res.render("login", { errors: ["You aren't a user..."] });
+    }
+    if (foundUser.password === reqPassword) {
+        delete foundUser.password;
+        req.session.user = foundUser;
+        res.redirect("/profile");
+    } else {
+        return res.render("login", { errors: ["That's not your password..."] });
+    }
+});
+
+app.get("/profile", checkAuth, (req, res) => {
+    res.render("profile", { user: req.session.user });
 });
